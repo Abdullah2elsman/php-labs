90639c466e0ece1201fc2a12dd199a835d9ce19c
Abdullah2elsman <abdullahelsman294@gmail.com>
2026-03-04 11:43:12 +0200

    upload lab 2

---
+++ list.php
@@ -0,0 +1,28 @@
+<?php
+
+$rows = file("users.txt");
+
+echo "<table border='1'>";
+echo "<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Actions</th>
+</tr>";
+
+foreach($rows as $index => $row){
+
+   $data = explode(",",$row);
+
+   echo "<tr>";
+   echo "<td>$index</td>";
+   echo "<td>".$data[0]." ".$data[1]."</td>";
+
+   echo "<td>
+   <a href='view.php?id=$index'>View</a>
+   <a href='delete.php?id=$index'>Delete</a>
+   </td>";
+
+   echo "</tr>";
+}
+
+echo "</table>";
